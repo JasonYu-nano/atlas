e55c485d4720c6bc81192ea25f3fc37bb6d3fbf0
JasonYu <fishyu7471@outlook.com>
2024-04-17 20:29:48 +0800

    feat: rebase to main

---
+++ src/rhi/include/rhi_types.hpp
@@ -0,0 +1,21 @@
+// Copyright(c) 2023-present, Atlas.
+// Distributed under the MIT License (http://opensource.org/licenses/MIT)
+
+#pragma once
+
+#include "core_def.hpp"
+
+namespace atlas
+{
+
+enum class ERhiBackend : uint8
+{
+    OpneGL,
+};
+
+class RHI_API RHIBackendInitParams
+{
+
+};
+
+}// namespace atlas
