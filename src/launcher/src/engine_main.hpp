709883caa80424c4cf4667aa67b2851ae947886a
JasonYu <fishyu7471@outlook.com>
2024-04-18 11:53:41 +0800

    feat: use different entry point on different platform

---
+++ src/launcher/src/engine_main.hpp
@@ -0,0 +1,11 @@
+// Copyright(c) 2023-present, Atlas.
+// Distributed under the MIT License (http://opensource.org/licenses/MIT)
+
+#pragma once
+
+namespace atlas
+{
+
+int EngineMain(int argc, char* argv[]);
+
+}// namespace atlas
