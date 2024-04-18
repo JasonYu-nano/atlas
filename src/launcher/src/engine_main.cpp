0180fae0c41e49127981315eecb9bcf0abe9f510
JasonYu <fishyu7471@outlook.com>
2024-04-18 11:53:41 +0800

    feat: use different entry point on different platform

--- src/launcher/src/engine_main.cpp
+++ src/launcher/src/engine_main.cpp
@@ -1,13 +1,24 @@
 // Copyright(c) 2023-present, Atlas.
 // Distributed under the MIT License (http://opensource.org/licenses/MIT)
 
+#include "engine_main.hpp"
 #include "game_engine.hpp"
+#include "misc/scope_guard.hpp"
 
-using namespace atlas;
+namespace atlas
+{
 
-int main()
+int EngineMain(int argc, char* argv[])
 {
     g_engine = new GameEngine();
+
+    using exit = decltype([]() {
+        g_engine->Shutdown();
+    });
+
+    // make sure can always call engine shutdown on program terminated.
+    ScopeGuard<exit> guard;
+
 
     g_engine->Startup();
 
@@ -18,8 +29,12 @@ int main()
 
     g_engine->Shutdown();
 
+    guard.Release();
+
     delete g_engine;
     g_engine = nullptr;
 
     return 0;
 }
+
+}// namespace atlas
---
+++ src/launcher/src/windows/main.cpp
@@ -0,0 +1,13 @@
+// Copyright(c) 2023-present, Atlas.
+// Distributed under the MIT License (http://opensource.org/licenses/MIT)
+
+#include "engine_main.hpp"
+
+using namespace atlas;
+
+int main(int argc, char* argv[])
+{
+    int error_code = EngineMain(argc, argv);
+
+    return error_code;
+}
