15ce3fdbd047e58a1fb8ce10a617f5dddc880139
Shinonome <fishyu7471@outlook.com>
2024-07-01 23:24:18 +0800

    feat: improve gl functions

--- src/rhi/src/opengl/opengl_functions.cpp
+++ src/rhi/src/opengl/opengl_functions.cpp
@@ -24,7 +24,7 @@ static void* resolve_symbol(const OpenGLContext& context, StringView fn_name)
         std::memcpy(fn, fn_name.data(), size);
         char *ext = fn + size;
 
-        for (const auto &e : extensions)
+        for (const auto& e : extensions)
         {
             std::memcpy(ext, e.data(), e.length());
             function = context.get_proc_address(fn);
@@ -39,13 +39,14 @@ static void* resolve_symbol(const OpenGLContext& context, StringView fn_name)
 }
 
 #define GLFN_FUNCTION_NAMES(ret, name, param, arg) "gl"#name"\0"
+#define GET_GLFN_COUNT(ret, name, param, arg) +1
 
 OpenGLFunctions::OpenGLFunctions(const OpenGLContext& context)
 {
-    const char* names = GLFN_FUNCTIONS(GLFN_FUNCTION_NAMES);
+    const char* names = FOREACH_GL_FUNCTIONS(GLFN_FUNCTION_NAMES);
     const char* name = names;
 
-    for (int i = 0; i < GLFN_FUNCTIONS(GLFN_COUNT_FUNCTIONS); ++i)
+    for (int i = 0; i < FOREACH_GL_FUNCTIONS(GET_GLFN_COUNT); ++i)
     {
         functions[i] = resolve_symbol(context, name);
         name += strlen(name) + 1;
@@ -56,7 +57,7 @@ void OpenGLFunctions::initialize()
 {
     // auto context = OpenGLContext::current_context();
 
-    const char* names = GLFN_FUNCTIONS(GLFN_FUNCTION_NAMES);
+    const char* names = FOREACH_GL_FUNCTIONS(GLFN_FUNCTION_NAMES);
     const char* name = names;
 
     // for (int i = 0; i < GLFN_FUNCTIONS(GLFN_COUNT_FUNCTIONS); ++i)
@@ -65,5 +66,8 @@ void OpenGLFunctions::initialize()
     //     name += strlen(name) + 1;
     // }
 }
+
+#undef GLFN_FUNCTION_NAMES
+#undef GET_GLFN_COUNT
 
 }// namespace atlas
