2bfafe74feb940f58a3525a1edfedf775091c32a
Shinonome <fishyu7471@outlook.com>
2024-09-22 23:09:06 +0800

    feat: add class serialize

---
+++ src/core/src/meta/property.cpp
@@ -0,0 +1,106 @@
+// Copyright(c) 2023-present, Atlas.
+// Distributed under the MIT License (http://opensource.org/licenses/MIT)
+
+#include "meta/property.hpp"
+#include "meta/class.hpp"
+
+namespace atlas
+{
+
+void Property::serialize(WriteStream& stream, const void* data) const
+{
+    if (!has_flag(EPropertyFlag::Temporary))
+    {
+        stream << name_;
+        serialize_impl(stream, data);
+    }
+}
+
+int64 IntProperty::get_value(const void* structure_ptr) const
+{
+    switch (property_size_)
+    {
+        case 1: return get_value_internal<int8>(structure_ptr);
+        case 2: return get_value_internal<int16>(structure_ptr);
+        case 4: return get_value_internal<int32>(structure_ptr);
+        case 8: return get_value_internal<int64>(structure_ptr);
+        default: std::unreachable();
+    }
+}
+
+void IntProperty::set_value(void* structure_ptr, int64 new_value) const
+{
+    switch (property_size_)
+    {
+        case 1: return set_value_internal<int8>(structure_ptr, new_value);
+        case 2: return set_value_internal<int16>(structure_ptr, new_value);
+        case 4: return set_value_internal<int32>(structure_ptr, new_value);
+        case 8: return set_value_internal<int64>(structure_ptr, new_value);
+        default: std::unreachable();
+    }
+}
+
+void IntProperty::serialize_impl(WriteStream& stream, const void* data) const
+{
+    switch (property_size_)
+    {
+        case 1: stream << get_value_internal<int8>(data); break;
+        case 2: stream << get_value_internal<int16>(data); break;
+        case 4: stream << get_value_internal<int32>(data); break;
+        case 8: stream << get_value_internal<int64>(data); break;
+        default: std::unreachable();
+    }
+}
+
+uint64 UIntProperty::get_value(const void* structure_ptr) const
+{
+    switch (property_size_)
+    {
+        case 1: return get_value_internal<uint8>(structure_ptr);
+        case 2: return get_value_internal<uint16>(structure_ptr);
+        case 4: return get_value_internal<uint32>(structure_ptr);
+        case 8: return get_value_internal<uint64>(structure_ptr);
+        default: std::unreachable();
+    }
+}
+
+void UIntProperty::set_value(void* structure_ptr, uint64 new_value) const
+{
+    switch (property_size_)
+    {
+        case 1: return set_value_internal<uint8>(structure_ptr, new_value);
+        case 2: return set_value_internal<uint16>(structure_ptr, new_value);
+        case 4: return set_value_internal<uint32>(structure_ptr, new_value);
+        case 8: return set_value_internal<uint64>(structure_ptr, new_value);
+        default: std::unreachable();
+    }
+}
+
+void UIntProperty::serialize_impl(WriteStream& stream, const void* data) const
+{
+    switch (property_size_)
+    {
+        case 1: stream << get_value_internal<uint8>(data); break;
+        case 2: stream << get_value_internal<uint16>(data); break;
+        case 4: stream << get_value_internal<uint32>(data); break;
+        case 8: stream << get_value_internal<uint64>(data); break;
+        default: std::unreachable();
+    }
+}
+
+void FloatPointProperty::serialize_impl(WriteStream& stream, const void* data) const
+{
+    switch (property_size_)
+    {
+        case 4: stream << get_value_internal<float>(data); break;
+        case 8: stream << get_value_internal<double>(data); break;
+        default: std::unreachable();
+    }
+}
+
+void ClassProperty::serialize_impl(WriteStream& stream, const void* data) const
+{
+    class_->serialize(stream, get_class_address(data));
+}
+
+}// namespace atlas
